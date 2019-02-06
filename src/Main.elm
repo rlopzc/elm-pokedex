741424f11a71a49898b890106996f71838587096
Romario López <romariolopezc@gmail.com>
2019-02-06 16:45:22 -0600

    App initialization and initial data modeling

--- src/Main.elm
+++ src/Main.elm
@@ -1,7 +1,71 @@
 module Main exposing (main)
 
+import Browser
 import Html exposing (..)
 
 
+
+-- MODEL
+
+
+type Model
+    = Loading
+    | Loaded Internals
+    | Errored String
+
+
+init : ( Model, Cmd Msg )
+init =
+    ( Loading, Cmd.none )
+
+
+
+-- TYPES
+
+
+type alias Internals =
+    { pokemonList : List Pokemon
+    }
+
+
+type alias Pokemon =
+    { name : String
+    }
+
+
+
+-- VIEW
+
+
+view : Model -> Html Msg
+view model =
+    text "Romario"
+
+
+
+-- UPDATE
+
+
+type Msg
+    = NoOp
+
+
+update : Msg -> Model -> ( Model, Cmd Msg )
+update msg model =
+    case msg of
+        NoOp ->
+            ( model, Cmd.none )
+
+
+
+-- PROGRAM
+
+
+main : Program () Model Msg
 main =
-    text "Initial commit"
+    Browser.element
+        { init = \_ -> init
+        , view = view
+        , update = update
+        , subscriptions = \_ -> Sub.none
+        }
